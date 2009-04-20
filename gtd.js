2308ed3ca05e4920e488c2daf4d3484704d5f2f2
Claes Holmerson <claes@knut.voip>
2009-04-20 19:21:00 +0200

    Some date stuff added

--- gtd.js
+++ gtd.js
@@ -1,10 +1,119 @@
+Date.prototype.toISO8601String = function (format, offset) {
+    /* accepted values for the format [1-6]:
+     1 Year:
+       YYYY (eg 1997)
+     2 Year and month:
+       YYYY-MM (eg 1997-07)
+     3 Complete date:
+       YYYY-MM-DD (eg 1997-07-16)
+     4 Complete date plus hours and minutes:
+       YYYY-MM-DDThh:mmTZD (eg 1997-07-16T19:20+01:00)
+     5 Complete date plus hours, minutes and seconds:
+       YYYY-MM-DDThh:mm:ssTZD (eg 1997-07-16T19:20:30+01:00)
+     6 Complete date plus hours, minutes, seconds and a decimal
+       fraction of a second
+       YYYY-MM-DDThh:mm:ss.sTZD (eg 1997-07-16T19:20:30.45+01:00)
+    */
+    if (!format) { var format = 6; }
+    if (!offset) {
+        var offset = 'Z';
+        var date = this;
+    } else {
+        var d = offset.match(/([-+])([0-9]{2}):([0-9]{2})/);
+        var offsetnum = (Number(d[2]) * 60) + Number(d[3]);
+        offsetnum *= ((d[1] == '-') ? -1 : 1);
+        var date = new Date(Number(Number(this) + (offsetnum * 60000)));
+    }
+
+    var zeropad = function (num) { return ((num < 10) ? '0' : '') + num; }
+
+    var str = "";
+    str += date.getUTCFullYear();
+    if (format > 1) { str += "-" + zeropad(date.getUTCMonth() + 1); }
+    if (format > 2) { str += "-" + zeropad(date.getUTCDate()); }
+    if (format > 3) {
+        str += "T" + zeropad(date.getUTCHours()) +
+               ":" + zeropad(date.getUTCMinutes());
+    }
+    if (format > 5) {
+        var secs = Number(date.getUTCSeconds() + "." +
+                   ((date.getUTCMilliseconds() < 100) ? '0' : '') +
+                   zeropad(date.getUTCMilliseconds()));
+        str += ":" + zeropad(secs);
+    } else if (format > 4) { str += ":" + zeropad(date.getUTCSeconds()); }
+
+    if (format > 3) { str += offset; }
+    return str;
+}
+
+Date.prototype.setISO8601 = function (string) {
+    var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
+        "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\.([0-9]+))?)?" +
+        "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?";
+    var d = string.match(new RegExp(regexp));
+
+    var offset = 0;
+    var date = new Date(d[1], 0, 1);
+
+    if (d[3]) { date.setMonth(d[3] - 1); }
+    if (d[5]) { date.setDate(d[5]); }
+    if (d[7]) { date.setHours(d[7]); }
+    if (d[8]) { date.setMinutes(d[8]); }
+    if (d[10]) { date.setSeconds(d[10]); }
+    if (d[12]) { date.setMilliseconds(Number("0." + d[12]) * 1000); }
+    if (d[14]) {
+        offset = (Number(d[16]) * 60) + Number(d[17]);
+        offset *= ((d[15] == '-') ? 1 : -1);
+    }
+
+    offset -= date.getTimezoneOffset();
+    time = (Number(date) + (offset * 60 * 1000));
+    this.setTime(Number(time));
+}
+
+var dates = [];
 
 function init_todo() {
-    var d = document.createElement("div");
-    d.setAttribute('id', 'tododiv');
-    $("#todo").wrap(d);
-    $("#tododiv").css("border","3px solid red")
-    $("#tododiv + p").css("border","3px solid green")
+    $("#div-todo > p").each(function() {
+	    var text = $(this).text();
+	    var todostatus = /^\[(.)\]/.exec(text);
+	    //Mark as todo item
+	    if (todostatus) {
+		if (todostatus.index != -1) {
+		    $(this).addClass("todo");
+		}
+		//kind of todo item
+		if (todostatus[1] == " ") {
+		    $(this).addClass("notstarted");
+		} else if (todostatus[1] == "+") {
+		    $(this).addClass("completed");
+		} else if (todostatus[1] == '-') {
+		    $(this).addClass("aborted");
+		} else if (todostatus[1] == '>') {
+		    $(this).addClass("inprocess");
+		} else if (todostatus[1] == '<') {
+		    $(this).addClass("halted");
+		} else if (todostatus[1] == '!') {
+		    $(this).addClass("needsaction");
+		} else if (todostatus[1] == '?') {
+		    $(this).addClass("unknownstatus");
+		} 
+
+		//Find dates
+		var date = /([0-9]{4,4}-[0-9]{2,2}-[0-9]{2,2})/.exec(text);
+		if (date && date.index != -1) {
+		    var datestring = date[1];
+		    var dateObj = new Date();
+		    dateObj.setISO8601(datestring);
+		    dates.push(dateObj);
+		}
+		
+
+	    }
+       	});
+    for (var i = 0; i < dates.length; i++) {
+	var date = dates[i];
+    }
 };
 
 function init_structure() {
@@ -17,6 +126,7 @@ function init_structure() {
         var nextHeader = headerList[i+1];
         var nextSibling = header.nextSibling;
         var div = document.createElement('div');
+	div.id = "div-" + header.id;
         header.parentNode.replaceChild(div, header);  
         div.appendChild(header);  
         while (nextSibling != nextHeader) {
@@ -30,6 +140,6 @@ function init_structure() {
 }
 
 $(document).ready(function(){
-	//init_todo();
 	init_structure();
+	init_todo();
  });
